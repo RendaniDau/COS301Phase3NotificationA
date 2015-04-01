d86270882f1f09932340dde0932552ad249aa9c4
Byron Dinkelmann <u11057638@tuks.co.za>
2015-04-01 15:27:36 +0200

    Added html format to email

--- email/emailNotification.js
+++ email/emailNotification.js
@@ -1,11 +1,11 @@
 var nodemailer = require('nodemailer');
+
 var inSender="Buzz Notification System";
 
 //Function to send email list to specified recipient
 module.exports = {
     sendNotification: function (inUser, inPassword, inRecipient, inNotificationType, inMessage)
     {
-
         var smtpTransport = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
@@ -18,7 +18,9 @@ module.exports = {
             from: inSender,
             to: inRecipient,
             subject: inNotificationType,
-            text: inMessage  //Messages to send
+            text: inMessage,  //Messages to send
+            html:'<head><title>'+inNotificationType+'</title></head><body>Good day '+inRecipient+',<br><br>'+inMessage+'<br><br>Thank you,<br><br>The Buzz System</body>'
+                        
         }, 
         function (error, response)
         {
